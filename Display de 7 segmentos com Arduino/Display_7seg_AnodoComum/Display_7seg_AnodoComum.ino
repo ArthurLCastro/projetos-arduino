7c453188951e7e6d854b579c206cee3cb984ded6
Arthur Castro <arthur.castro07@gmail.com>
2017-10-16 10:29:06 -0300

    Upload de códigos

---
+++ Display de 7 segmentos com Arduino/Display_7seg_AnodoComum/Display_7seg_AnodoComum.ino
@@ -0,0 +1,42 @@
+// Display de 7 segmentos - Ânodo Comum
+// Arthur L. Castro
+// Outubro de 2017
+
+#define tempo 500
+byte i, n, digito = 0;     // Variáveis auxiliares
+byte portas[8] = {2,3,4,5,6,7,8,9};
+                           // a,b,c,d,e,f,g,dp
+byte displayHex[16][8] = {{0,0,0,0,0,0,1,1},  // 0
+                          {1,0,0,1,1,1,1,1},  // 1
+                          {0,0,1,0,0,1,0,1},  // 2
+                          {0,0,0,0,1,1,0,1},  // 3
+                          {1,0,0,1,1,0,0,1},  // 4
+                          {0,1,0,0,1,0,0,1},  // 5
+                          {0,1,0,0,0,0,0,1},  // 6
+                          {0,0,0,1,1,1,1,1},  // 7
+                          {0,0,0,0,0,0,0,1},  // 8
+                          {0,0,0,0,1,0,0,1},  // 9
+                          {0,0,0,1,0,0,0,1},  // A
+                          {1,1,0,0,0,0,0,1},  // b
+                          {0,1,1,0,0,0,1,1},  // C
+                          {1,0,0,0,0,1,0,1},  // d
+                          {0,1,1,0,0,0,0,1},  // E
+                          {0,1,1,1,0,0,0,1},  // F
+                         };
+                            
+void setup() {
+  for(i=0;i<=7;i++){
+    pinMode(portas[i],OUTPUT);
+  }
+}
+
+void loop() {
+  if(digito > 15){
+    digito = 0;
+  }
+  for(n=0;n<=7;n++){
+    digitalWrite(portas[n],displayHex[digito][n]); 
+  }
+  digito++;
+  delay(tempo);
+}
---
+++ Display de 7 segmentos com Arduino/Display_7seg_CatodoComum/Display_7seg_CatodoComum.ino
@@ -0,0 +1,42 @@
+// Display de 7 segmentos - Cátodo Comum
+// Arthur L. Castro
+// Outubro de 2017
+
+#define tempo 500
+byte i, n, digito = 0;     // Variáveis auxiliares
+byte portas[8] = {2,3,4,5,6,7,8,9};
+                           // a,b,c,d,e,f,g,dp
+byte displayHex[16][8] = {{1,1,1,1,1,1,0,0},  // 0
+                          {0,1,1,0,0,0,0,0},  // 1
+                          {1,1,0,1,1,0,1,0},  // 2
+                          {1,1,1,1,0,0,1,0},  // 3
+                          {0,1,1,0,0,1,1,0},  // 4
+                          {1,0,1,1,0,1,1,0},  // 5
+                          {1,0,1,1,1,1,1,0},  // 6
+                          {1,1,1,0,0,0,0,0},  // 7
+                          {1,1,1,1,1,1,1,0},  // 8
+                          {1,1,1,1,0,1,1,0},  // 9
+                          {1,1,1,0,1,1,1,0},  // A
+                          {0,0,1,1,1,1,1,0},  // b
+                          {1,0,0,1,1,1,0,0},  // C
+                          {0,1,1,1,1,0,1,0},  // d
+                          {1,0,0,1,1,1,1,0},  // E
+                          {1,0,0,0,1,1,1,0},  // F
+                         };
+                            
+void setup() {
+  for(i=0;i<=7;i++){
+    pinMode(portas[i],OUTPUT);
+  }
+}
+
+void loop() {
+  if(digito > 15){
+    digito = 0;
+  }
+  for(n=0;n<=7;n++){
+    digitalWrite(portas[n],displayHex[digito][n]); 
+  }
+  digito++;
+  delay(tempo);
+}
