44b37633f921f4422a2686b3512472adf02f9b45
Arthur Castro <arthur.castro07@gmail.com>
2018-04-08 11:49:47 -0300

    Upload de "Temp-Umid-Lum-Serial.ino"

---
+++ Alimentar MySQL com Serial - Python/Temp-Umid-Lum-Serial/Temp-Umid-Lum-Serial.ino
@@ -0,0 +1,36 @@
+// Alimentar MySQL com dados lidos da Porta Serial 2
+// Arthur Castro
+// 08 de abril de 2018
+
+#include <DHT.h>
+
+#define LUM_PIN A0
+#define tempo 500               //Tempo entre leituras (em ms)
+#define DHTPIN D3
+#define DHTTYPE DHT22
+
+String sinais;                  // Variável usada para enviar os sinais
+int lum = 0, lumPercent = 0;
+
+DHT dht(DHTPIN, DHTTYPE);
+float temp;
+float umid;
+
+void setup() {
+  pinMode(LUM_PIN, INPUT);      // Define A0 como entrada
+  dht.begin();
+  Serial.begin(9600);           //Inicia comunicação serial
+}
+ 
+void loop() {
+  lum = analogRead(LUM_PIN);
+  lumPercent = map(lum,0,1023,0,100);
+  
+  temp = dht.readTemperature();
+  umid = dht.readHumidity();
+
+  sinais = String(lumPercent) + "|" + String(temp) + "|" + String(umid);
+  Serial.println(sinais);
+
+  delay(tempo);
+}
